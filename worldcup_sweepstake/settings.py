99e2ffe80d6d35848455a7ac932f25f48409b146
Santiago Palomares Gallego <santiago@MacBook-Air-de-Santiago.local>
2026-06-12 22:38:24 +0200

    Added os and django-env imports and updated STATIC_ROOT

--- worldcup_sweepstake/settings.py
+++ worldcup_sweepstake/settings.py
@@ -1,4 +1,6 @@
+import os
 from pathlib import Path
+import environ
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -71,6 +73,6 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login/'
